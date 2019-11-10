607ef61b436e1b27baccbf7f9e3a9029c519f702
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2019-11-10 11:04:55 +0100

    Add files via upload

---
+++ src/SI1145_WE.cpp
@@ -0,0 +1,337 @@
+/*****************************************
+* This is a library for the SI1145 ALS, IR, UV and PS Sensor Module
+*
+* You'll find an example which should enable you to use the library. 
+*
+* You are free to use it, change it or build on it. In case you like 
+* it, it would be cool if you give it a star.
+* 
+* If you find bugs, please inform me!
+* 
+* Written by Wolfgang (Wolle) Ewald
+* https://wolles-elektronikkiste.de
+*
+*******************************************/
+
+#include "SI1145_WE.h"
+
+SI1145_WE::SI1145_WE(){
+	i2cAddress = 0x60;
+}
+	
+void SI1145_WE::init(){	
+	/* sets default parameters */
+		
+	resetSI1145();
+	
+	/* UV default calibration values */
+	setRegister(SI1145_REG_UCOEFF0, 0x29);
+	setRegister(SI1145_REG_UCOEFF1, 0x89);
+	setRegister(SI1145_REG_UCOEFF2, 0x02);
+	setRegister(SI1145_REG_UCOEFF3, 0x00);
+	
+	/* Interrupt Settings */
+	enableInterrupt(PSALS_INT);
+	
+	/* pause between measurements =  31.25 µs x rate */
+	setMeasurementRate(0x00FF);
+	
+	/* set LED Current, 22.4 mA */
+	setLEDCurrent(3); 
+	
+	/* prox sensor 1 uses LED1 */
+	setParameter(SI1145_PARA_PSLED12_SELECT, 0x01);
+	
+	/* PS Photodiode Selection */
+	selectPsDiode(LARGE_DIODE);
+	
+	/* Pulse Width Selection of IR LED pulse; pulse width = pulse width * 2^PS_ADC_Gain 
+		No value > 5 shall be taken */
+	setPsAdcGain(0);
+	disableHighSignalPsRange();
+	
+	/* ALS-IR Photodiode Selection */
+	selectIrDiode(SMALL_DIODE);
+	
+	/* Integration time for ALS-VIS measurements = integration time * 2^ALS_VIS_ADC_GAIN 
+		No value > 7 (factor 128) shall be taken */
+	setAlsVisAdcGain(0);
+	disableHighSignalVisRange();
+	
+	/* Integration time for ALS-IR measurements = integration time * 2^ALS_IR_ADC_GAIN 
+		No value > 7 (factor 128) shall be taken */
+	setAlsIrAdcGain(0);
+	disableHighSignalIrRange();
+}
+
+void SI1145_WE::resetSI1145(){
+	setRegister(SI1145_REG_COMMAND, SI1145_RESET);
+	delay(10);
+	setHwKey();
+	delay(10);
+}
+
+void SI1145_WE::setI2CAddress(int addr){
+	setParameter(SI1145_PARA_I2C_ADDR, (uint8_t)addr); delay(10);
+	setRegister(SI1145_REG_COMMAND, SI1145_BUSADDR); delay(10);
+	i2cAddress = addr;
+}
+
+void SI1145_WE::enableMeasurements(SI1145MeasureType t, SI1145MeasureMode m){
+	deviceMeasureMode = m;
+	deviceMeasureType = t;
+	uint8_t chListValue = 0;
+	uint8_t typeAndMode = 0;
+	
+	uint8_t parameter; 
+	switch(t){
+		case PS:
+			chListValue = 0b00000001;
+			break;
+		case ALS:
+			chListValue = 0b00110000;   // IR wird immer mit ALS zusammen aktiviert
+			break;
+		case PSALS:
+			chListValue = 0b00110001;
+			break;
+		case ALSUV:
+			chListValue = 0b10110000;
+			break;
+		case PSALSUV:
+			chListValue = 0b10110001;
+			break;
+	}
+	typeAndMode = m | t; 
+	setParameter(SI1145_PARA_CHLIST, chListValue);
+	setRegister(SI1145_REG_COMMAND, typeAndMode);
+	delay(10);
+}
+
+void SI1145_WE::startSingleMeasurement(){
+	uint8_t typeAndMode = deviceMeasureMode | deviceMeasureType;
+	setRegister(SI1145_REG_COMMAND, typeAndMode);
+}
+
+void SI1145_WE::enableInterrupt(SI1145IntType t){
+	setRegister(SI1145_REG_INT_CFG, 1);
+	setRegister(SI1145_REG_IRQEN, t);
+}
+
+void SI1145_WE::disableAllInterrupts(){
+	setRegister(SI1145_REG_INT_CFG, 0);
+}
+
+void SI1145_WE::setMeasurementRate(uint16_t rate){
+	setRegister16bit(SI1145_REG_MEASRATE0, rate);
+}
+
+void SI1145_WE::setHwKey(){
+	setRegister(SI1145_REG_HW_KEY, 0x17);
+}
+
+void SI1145_WE::setLEDCurrent(uint8_t curr){
+	setRegister(SI1145_REG_PSLED21, curr);
+}
+
+void SI1145_WE::selectPsDiode(uint8_t diode){
+	setParameter(SI1145_PARA_PS1_ADCMUX, diode);
+}
+
+void SI1145_WE::selectIrDiode(uint8_t diode){
+	setParameter(SI1145_PARA_ALS_IR_ADCMUX, diode);
+}
+
+void SI1145_WE::enableHighResolutionPs(){
+	setParameter(SI1145_PARA_PS_ENCODING, 0b0001000);
+}
+
+void SI1145_WE::disableHighResolutionPs(){
+	setParameter(SI1145_PARA_PS_ENCODING, 0b0000000);
+}
+
+void SI1145_WE::enableHighResolutionVis(){
+	uint8_t ALSEncodingStatus = getParameter(SI1145_PARA_ALS_ENCODING);
+	ALSEncodingStatus |= 0b00010000;
+	setParameter(SI1145_PARA_ALS_ENCODING, ALSEncodingStatus);
+}
+
+void SI1145_WE::disableHighResolutionVis(){
+	uint8_t ALSEncodingStatus = getParameter(SI1145_PARA_ALS_ENCODING);
+	ALSEncodingStatus &= 0b11101111;
+	setParameter(SI1145_PARA_ALS_ENCODING, 0b0000000);
+}
+
+void SI1145_WE::enableHighResolutionIr(){
+	uint8_t ALSEncodingStatus = getParameter(SI1145_PARA_ALS_ENCODING);
+	ALSEncodingStatus |= 0b00100000;
+	setParameter(SI1145_PARA_ALS_ENCODING, ALSEncodingStatus);
+}
+
+void SI1145_WE::disableHighResolutionIr(){
+	uint8_t ALSEncodingStatus = getParameter(SI1145_PARA_ALS_ENCODING);
+	ALSEncodingStatus &= 0b11011111;
+	setParameter(SI1145_PARA_ALS_ENCODING, ALSEncodingStatus);
+}
+
+void SI1145_WE::setPsAdcGain(byte psAdcGain){      /* psAdcGain <= 5! */
+	setParameter(SI1145_PARA_PS_ADC_GAIN, psAdcGain);
+	/* data sheet recommendation: psAdcRec = one's complement of psAdcGain */
+	byte psAdcRec = ((~psAdcGain) & 0b00000111) << 4; 
+	setParameter(SI1145_PARA_PS_ADCOUNTER, psAdcRec);
+}
+
+void SI1145_WE::enableHighSignalPsRange(){
+	setParameter(SI1145_PARA_PS_ADC_MISC, 0x24); // change of PS_ADC_MODE not implemented
+}
+	
+void SI1145_WE::disableHighSignalPsRange(){
+	setParameter(SI1145_PARA_PS_ADC_MISC, 0x04); // change of PS_ADC_MODE not implemented
+}
+
+void SI1145_WE::setAlsVisAdcGain(byte alsVisAdcGain){    /* alsVisAdcGain <= 7 */
+	setParameter(SI1145_PARA_ALS_VIS_ADC_GAIN, alsVisAdcGain);
+	/* data sheet recommendation: visAdcRec = one's complement of alsVisAdcGain */
+	byte visAdcRec = ((~alsVisAdcGain) & 0b00000111) << 4;
+	setParameter(SI1145_PARA_ALS_VIS_ADC_COUNTER, visAdcRec);
+}
+
+void SI1145_WE::enableHighSignalVisRange(){
+	setParameter(SI1145_PARA_ALS_VIS_ADC_MISC, 0x20); // change of PS_ADC_MODE not implemented
+}
+	
+void SI1145_WE::disableHighSignalVisRange(){
+	setParameter(SI1145_PARA_ALS_VIS_ADC_MISC, 0x00); // change of PS_ADC_MODE not implemented
+}
+
+void SI1145_WE::setAlsIrAdcGain(byte alsIrAdcGain){      /* irAdcGain <= 7 */
+	setParameter(SI1145_PARA_ALS_IR_ADC_GAIN, alsIrAdcGain);
+	/* data sheet recommendation: irAdcRec = one's complement of alsIrAdcGain */
+	byte irAdcRec = ((~alsIrAdcGain) & 0b00000111) << 4; 
+	setParameter(SI1145_PARA_ALS_IR_ADC_COUNTER, irAdcRec);
+}
+
+void SI1145_WE::enableHighSignalIrRange(){
+	setParameter(SI1145_PARA_ALS_IR_ADC_MISC, 0x20); 
+}
+	
+void SI1145_WE::disableHighSignalIrRange(){
+	setParameter(SI1145_PARA_ALS_IR_ADC_MISC, 0x00); 
+}
+
+uint16_t SI1145_WE::getAlsVisData(){
+	return getRegister16bit(SI1145_REG_ALS_VIS_DATA0);
+}
+
+uint16_t SI1145_WE::getAlsIrData(){
+	return getRegister16bit(SI1145_REG_ALS_IR_DATA0);
+}
+
+uint16_t SI1145_WE::getPsData(){
+	return getRegister16bit(SI1145_REG_PS1_DATA0);
+}
+
+float SI1145_WE::getUvIndex(){
+	float uvi = getRegister16bit(SI1145_REG_UV_INDEX0)/100.0;
+	return uvi;
+}
+
+uint8_t SI1145_WE::getFailureMode(){
+	return getRegister(SI1145_REG_RESPONSE);
+}
+
+void SI1145_WE::clearFailure(){
+	setRegister(SI1145_REG_COMMAND, SI1145_NOP);
+}
+
+void SI1145_WE::clearAllInterrupts(){
+	setRegister(SI1145_REG_IRQ_STAT, 0xFF);
+}
+
+void SI1145_WE::clearAlsInterrupt(){
+	setRegister(SI1145_REG_IRQ_STAT, ALS_INT);
+}
+
+void SI1145_WE::clearPsInterrupt(){
+	setRegister(SI1145_REG_IRQ_STAT, PS_INT);
+}
+
+void SI1145_WE::clearCmdInterrupt(){
+	setRegister(SI1145_REG_IRQ_STAT, CMD_INT);
+}
+
+uint8_t SI1145_WE::getInterruptStatus(){
+	return getRegister(SI1145_REG_IRQ_STAT);
+}
+
+
+/************************************************ 
+	private functions
+*************************************************/
+
+uint8_t SI1145_WE::setParameter(uint8_t param, uint8_t cmd){
+	param |= SI1145_PARAM_SET;
+	setRegister(SI1145_REG_PARAM_WR, cmd);
+	setRegister(SI1145_REG_COMMAND, param);
+	return getRegister(SI1145_REG_PARAM_RD);
+}
+
+uint8_t SI1145_WE::getParameter(uint8_t param){
+	param |= SI1145_PARAM_QUERY;
+	setRegister(SI1145_REG_COMMAND, param);
+	return getRegister(SI1145_REG_PARAM_RD);
+}
+
+
+uint8_t SI1145_WE::getRegister(uint8_t registerAddr)
+{
+	uint8_t data;
+
+	Wire.beginTransmission(i2cAddress); 
+	Wire.write(registerAddr); 
+	Wire.endTransmission(false); 
+	Wire.requestFrom(i2cAddress , 1);
+	data = Wire.read(); 
+
+	return data;
+}
+
+uint16_t SI1145_WE::getRegister16bit(uint8_t registerAddr)
+{
+	uint8_t data_low;
+	uint8_t data_high;
+	uint16_t data;
+
+	Wire.beginTransmission(i2cAddress); 
+	Wire.write(registerAddr); 
+	Wire.endTransmission(false); 
+
+	Wire.requestFrom(i2cAddress, 2);
+	data_low = Wire.read(); 
+	data_high = Wire.read(); 
+	data = (data_high << 8)|data_low;
+
+	return data;
+}
+
+void SI1145_WE::setRegister(uint8_t registerAddr, uint8_t data)
+{
+	Wire.beginTransmission(i2cAddress); 
+	Wire.write(registerAddr); 
+	Wire.write(data); 
+	Wire.endTransmission(); 
+}
+
+void SI1145_WE::setRegister16bit(uint8_t registerAddr, uint16_t data)
+{
+	Wire.beginTransmission(i2cAddress); 
+	Wire.write(registerAddr); 
+	uint8_t temp;
+	temp = data & 0xff;
+	Wire.write(temp); 
+	temp = (data >> 8) & 0xff;
+	Wire.write(temp); 
+	Wire.endTransmission(); 
+}
+	
+
+
---
+++ src/SI1145_WE.h
@@ -0,0 +1,182 @@
+/******************************************************************************
+ *
+ * This is a library for the SI1145 ALS, IR, UV and PS Sensor Module
+ *
+ * You'll find several example sketches which should enable you to use the library. 
+ *
+ * You are free to use it, change it or build on it. In case you like it, it would
+ * be cool if you give it a star.
+ *
+ * If you find bugs, please inform me!
+ * 
+ * Written by Wolfgang (Wolle) Ewald
+ * https://wolles-elektronikkiste.de
+ *
+ * 
+ ******************************************************************************/
+
+#ifndef SI1145_WE_H_
+#define SI1145_WE_H_
+
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+/* registers */
+#define SI1145_REG_PART_ID  		0x00
+#define SI1145_REG_REV_ID  			0x01
+#define SI1145_REG_SEQ_ID  			0x02
+#define SI1145_REG_INT_CFG  		0x03
+#define SI1145_REG_IRQEN  			0x04
+#define SI1145_REG_HW_KEY			0x07
+#define SI1145_REG_MEASRATE0 		0x08
+#define SI1145_REG_MEASRATE1  		0x09
+#define SI1145_REG_PSLED21  		0x0F
+#define SI1145_REG_UCOEFF0  		0x13
+#define SI1145_REG_UCOEFF1  		0x14
+#define SI1145_REG_UCOEFF2  		0x15
+#define SI1145_REG_UCOEFF3  		0x16
+#define SI1145_REG_PARAM_WR  		0x17
+#define SI1145_REG_COMMAND  		0x18
+#define SI1145_REG_RESPONSE  		0x20
+#define SI1145_REG_IRQ_STAT  	 	0x21
+#define SI1145_REG_ALS_VIS_DATA0 	0x22
+#define SI1145_REG_ALS_VIS_DATA1 	0x23
+#define SI1145_REG_ALS_IR_DATA0 	0x24
+#define SI1145_REG_ALS_IR_DATA1 	0x25
+#define SI1145_REG_PS1_DATA0 		0x26
+#define SI1145_REG_PS1_DATA1 		0x27
+#define SI1145_REG_UV_INDEX0 		0x2C
+#define SI1145_REG_UV_INDEX1 		0x2D
+#define SI1145_REG_PARAM_RD 		0x2E
+#define SI1145_REG_CHIP_STAT 		0x30
+
+/* commands */
+#define SI1145_PARAM_QUERY	0b10000000
+#define SI1145_PARAM_SET 	0b10100000
+#define SI1145_NOP 			0b00000000
+#define SI1145_RESET    	0b00000001
+#define SI1145_BUSADDR    	0b00000010
+#define SI1145_GET_CAL    	0b00010010
+
+/* response register error codes */
+#define SI1145_RESP_NO_ERROR				0b00000000
+#define SI1145_RESP_INVALID_SETTING			0b10000000
+#define SI1145_RESP_PS1_ADC_OVERFLOW		0b10001000
+#define SI1145_RESP_ALS_VIS_ADC_OVERFLOW	0b10001100
+#define SI1145_RESP_ALS_IR_ADC_OVERFLOW		0b10001101
+#define SI1145_RESP_AUX_ADC_OVERFLOW		0b10001110
+
+/* parameters */
+#define SI1145_PARA_I2C_ADDR 			0x00
+#define SI1145_PARA_CHLIST   			0x01
+#define SI1145_PARA_PSLED12_SELECT		0x02
+#define SI1145_PARA_PS_ENCODING			0x05
+#define SI1145_PARA_ALS_ENCODING  		0x06
+#define SI1145_PARA_PS1_ADCMUX   		0x07
+#define SI1145_PARA_PS_ADCOUNTER   		0x0A
+#define SI1145_PARA_PS_ADC_GAIN 		0x0B
+#define SI1145_PARA_PS_ADC_MISC 		0x0C
+#define SI1145_PARA_ALS_IR_ADCMUX 		0x0E
+#define SI1145_PARA_AUX_ADCMUX   		0x0F
+#define SI1145_PARA_ALS_VIS_ADC_COUNTER	0x10
+#define SI1145_PARA_ALS_VIS_ADC_GAIN 	0x11
+#define SI1145_PARA_ALS_VIS_ADC_MISC 	0x12
+#define SI1145_PARA_LED_REC				0x1C
+#define SI1145_PARA_ALS_IR_ADC_COUNTER  0x1D
+#define SI1145_PARA_ALS_IR_ADC_GAIN 	0x1E
+#define SI1145_PARA_ALS_IR_ADC_MISC 	0x1F
+
+/* diode selection */
+#define SMALL_DIODE  0x00
+#define LARGE_DIODE  0x03
+
+typedef enum SI1145MeasureMode{
+	FORCE = 0b00000100,   
+	PAUSE = 0b00001000,
+	AUTO  = 0b00001100
+} mode;
+
+typedef enum SI1145MeasureType{
+	PS 		= 0b00000001,
+	ALS		= 0b00000010,
+	PSALS	= 0b00000011,
+	ALSUV 	= 0b10000010,
+	PSALSUV = 0b10000011
+} type;
+
+typedef enum SI1145IntType{
+	ALS_INT = 0x01,
+	PS_INT	= 0x04,
+	PSALS_INT = 0x05,
+	CMD_INT = 0x20
+} intType;
+
+class SI1145_WE
+{
+public:	
+	SI1145_WE();
+  
+	void init();
+	void resetSI1145();
+	void setI2CAddress(int);
+	void enableMeasurements(SI1145MeasureType t, SI1145MeasureMode m);
+	void startSingleMeasurement();
+	void enableInterrupt(SI1145IntType t);
+	void disableAllInterrupts();
+	void setMeasurementRate(uint16_t rate);
+	void setLEDCurrent(uint8_t curr);
+	void selectPsDiode(uint8_t diode);
+	void selectIrDiode(uint8_t diode);
+	void enableHighResolutionPs();
+	void disableHighResolutionPs();
+	void enableHighResolutionVis();
+	void disableHighResolutionVis();
+	void enableHighResolutionIr();
+	void disableHighResolutionIr();
+	void setPsAdcGain(byte psAdcGain);
+	void enableHighSignalPsRange();
+	void disableHighSignalPsRange();
+	void setAlsVisAdcGain(byte alsVisAdcGain);
+	void enableHighSignalVisRange();
+	void disableHighSignalVisRange();
+	void setAlsIrAdcGain(byte alsIrAdcGain);
+	void enableHighSignalIrRange();
+	void disableHighSignalIrRange();
+	uint16_t getAlsVisData();
+	uint16_t getAlsIrData();
+	uint16_t getPsData();
+	float getUvIndex();
+	uint8_t getFailureMode();
+	void clearFailure();
+	void clearAllInterrupts();
+	void clearAlsInterrupt();
+	void clearPsInterrupt();
+	void clearCmdInterrupt();
+	uint8_t getInterruptStatus();
+
+private:
+
+	SI1145MeasureMode deviceMeasureMode; 
+	SI1145MeasureType deviceMeasureType;
+	int i2cAddress;
+	
+	void setHwKey();
+	
+	uint8_t setParameter(uint8_t param, uint8_t cmd);
+	uint8_t getParameter(uint8_t param);
+	
+	uint8_t getRegister(uint8_t registerAddr);
+	uint16_t getRegister16bit(uint8_t registerAddr);
+
+	void setRegister(uint8_t registerAddr, uint8_t data);
+	void setRegister16bit(uint8_t registerAddr, uint16_t data);
+
+};
+
+#endif
+
