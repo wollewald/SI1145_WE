2004246a6c8cfaeedcc13f9c7c0c549c1bdc4573
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-11-14 12:31:19 +0100

    Update SI1145_lux_calculation.ino

--- examples/SI1145_lux_calculation/SI1145_lux_calculation.ino
+++ examples/SI1145_lux_calculation/SI1145_lux_calculation.ino
@@ -8,7 +8,7 @@
 * the light measurement is performed 50 times. 
 * 
 * Further information can be found on:
-* https://wolles-elektronikkiste.de/si1145
+* https://wolles-elektronikkiste.de/si1145-als-ir-uv-und-ps-sensor?lang=en
 * 
 ***************************************************************************/
 
