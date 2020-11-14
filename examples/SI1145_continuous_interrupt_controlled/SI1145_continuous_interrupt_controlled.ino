cf47b3be056bdda3765d886b35a6b69b2ba5c8b6
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-11-14 12:30:25 +0100

    Update SI1145_continuous_interrupt_controlled.ino

--- examples/SI1145_continuous_interrupt_controlled/SI1145_continuous_interrupt_controlled.ino
+++ examples/SI1145_continuous_interrupt_controlled/SI1145_continuous_interrupt_controlled.ino
@@ -6,7 +6,7 @@
 * rate.  
 * 
 * Further information can be found on:
-* https://wolles-elektronikkiste.de/si1145
+* https://wolles-elektronikkiste.de/si1145-als-ir-uv-und-ps-sensor?lang=en
 * 
 ***************************************************************************/
 
