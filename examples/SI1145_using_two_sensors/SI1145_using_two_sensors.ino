e59bd40296bf48ee42c8e0835b3c1298082e3504
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-11-29 11:41:10 +0100

    Update SI1145_using_two_sensors.ino

--- examples/SI1145_using_two_sensors/SI1145_using_two_sensors.ino
+++ examples/SI1145_using_two_sensors/SI1145_using_two_sensors.ino
@@ -4,7 +4,7 @@
 * This sketch shows how to use two SI1145 sensors.
 *  
 * Further information can be found on:
-* https://wolles-elektronikkiste.de/si1145-als-ir-uv-und-ps-sensor?lang=en
+* https://wolles-elektronikkiste.de/en/si1145-als-ir-uv-und-ps-sensor
 * 
 ***************************************************************************/
 
