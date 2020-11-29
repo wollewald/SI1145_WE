6b0c818c46bd738870c30df1366dfc729f8a7fda
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-11-29 11:39:39 +0100

    Update SI1145_basics_continuous.ino

--- examples/SI1145_basics_continuous/SI1145_basics_continuous.ino
+++ examples/SI1145_basics_continuous/SI1145_basics_continuous.ino
@@ -4,7 +4,7 @@
 * This sketch shows how to do PS, ALS, VIS an UV measurements in continuous mode.
 *  
 * Further information can be found on:
-* https://wolles-elektronikkiste.de/si1145-als-ir-uv-und-ps-sensor?lang=en
+* https://wolles-elektronikkiste.de/en/si1145-als-ir-uv-und-ps-sensor
 * 
 ***************************************************************************/
 
