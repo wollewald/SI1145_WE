c81cb853842c5bced89d82664967bc88addba3bf
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-10-26 23:39:22 +0200

    Update SI1145_lux_calculation.ino

--- examples/SI1145_lux_calculation/SI1145_lux_calculation.ino
+++ examples/SI1145_lux_calculation/SI1145_lux_calculation.ino
@@ -17,8 +17,8 @@
 const int interruptPin = 2;
 
 SI1145_WE mySI1145 = SI1145_WE();
-// You can also pass a Wire object like wire2:
-// SI1145_WE mySI1145 = SI1145_WE(&wire2);
+// You can also pass a Wire object:
+// SI1145_WE mySI1145 = SI1145_WE(&Wire);
 
 void setup() {
   Serial.begin(9600);
